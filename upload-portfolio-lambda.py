ca77123a9ad2e54b7be17d71bd97439f4fa31d99
etacalpha <sbeard@intellicog.com>
2019-06-21 21:47:35 -0400

    changed python script to aws lambda function

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -1,19 +1,24 @@
+import json
 import boto3
 import io
 import zipfile
 import mimetypes
 
-s3 = boto3.resource('s3')
 
-pb = s3.Bucket('steven-beard.io')
-bb = s3.Bucket('steven-beard.io-build')
+def lambda_handler(event, context):
+    s3 = boto3.resource('s3')
 
-pz = io.BytesIO()
-bb.download_fileobj('portfolio_build.zip', pz)
+    pb = s3.Bucket('steven-beard.io')
+    bb = s3.Bucket('steven-beard.io-build')
 
-with zipfile.ZipFile(pz) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        pb.upload_fileobj(obj, nm, ExtraArgs={
-                          'ContentType': mimetypes.guess_type(nm)[0]})
-        pb.Object(nm).Acl().put(ACL='public-read')
+    pz = io.BytesIO()
+    bb.download_fileobj('portfolio_build.zip', pz)
+
+    with zipfile.ZipFile(pz) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            pb.upload_fileobj(obj, nm, ExtraArgs={
+                              'ContentType': mimetypes.guess_type(nm)[0]})
+            pb.Object(nm).Acl().put(ACL='public-read')
+
+    return 'Hello from Lambda'
